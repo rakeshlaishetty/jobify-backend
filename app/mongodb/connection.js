e51d17048889ba6747fc30b4c8f744183c0503f2
manoj kumar <manoj.kumar@rstartec.com>
2024-10-02 10:47:51 +0530

    db connected

---
+++ app/mongodb/connection.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { MONGODB_SECRET } = require("../../config/Constants");
+
+const MONGO_URL = `mongodb+srv://${MONGODB_SECRET.USER_NAME}:${MONGODB_SECRET.PASSWORD}@jobify.tgpcb.mongodb.net/`;
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+        });
+
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
---
+++ config/Constants.js
@@ -0,0 +1,8 @@
+export const MONGODB_SECRET = {
+    USER_NAME: process.env.MONGO_USERNAME,
+    PASSWORD: process.env.MONGO_PASSWORD
+}
+
+export const SERVER_CREDS = {
+    PORT_NUMBER : process.env.PORT_NUMBER || 8080
+}
