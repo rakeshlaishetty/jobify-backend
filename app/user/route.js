661953903d374f78f51c0696178619a91f5306cd
manoj kumar <manoj.kumar@rstartec.com>
2024-10-03 22:13:15 +0530

    Admin user can add delete update

--- app/user/route.js
+++ app/user/route.js
@@ -1,7 +1,9 @@
-const { AddUserAdmin } = require("./controller")
+const { AddUserAdmin,updateUser,deleteUser } = require("./controller")
 
 const router = require("express").Router()
 
 router.post("/add",AddUserAdmin)
+router.post("/update",updateUser)
+router.post("/removeuser",deleteUser)
 
 module.exports = router
